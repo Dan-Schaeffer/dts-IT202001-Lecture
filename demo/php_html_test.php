f4006b2c8ae9d0fb03f998d8ae47bf891c423510
Dan Schaeffer <dts@njit>
2025-09-16 13:49:49 -0400

    adding main and html test

---
+++ demo/php_html_test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+ <body>
+  <h1>This is a test of PHP code</h1>
+   <?php
+    echo "<p>This text was dynamically generated!</p>";
+   ?>
+   <h2>This is a simplistic version</h2>
+  <h1>This is the end of the test</h1>
+ </body>
+</html>
